a60b66b77e954dc973f5c7ba513a2f51db7b2931
jessicamcg <jessicamcguico@gmail.com>
2021-09-23 15:13:12 -0500

    integer validation debug and deleted extra 'readme'

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,7 @@ generateBtn.addEventListener("click", writePassword);
 
 // my code
 
-var spchar = ['!','@','#','$','%','^','&','*','_','-','?'];
+var spchar = ['!','@','#','$','%','^','&','*','_','-','?','(',')','+','.','/',':',';','<','>','=','[',']','^','~'];
 var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var nums = ['1','2','3','4','5','6','7','8','9','0'];
@@ -23,13 +23,9 @@ var nums = ['1','2','3','4','5','6','7','8','9','0'];
 function generatePassword() {
   var possiblePassword = [];
   var length = window.prompt('Length of password: ');
-  
-  while (length < 8 || length > 128) {
-    length = window.prompt('Password must be at least 8 characters long and less than 128: ');
-  };
 
-  while (Number.isInteger(length) === false) {
-    length = parseInt(window.prompt('Password length must be a number: '));
+  while (length < 8 || length > 128 || !Number.isInteger(Number(length))) {
+    length = prompt("Password length must be an integer between 8 and 128 characters. Please try again: ");
   };
 
   var checkspchar = window.confirm('Do you want to include special characters? ');
